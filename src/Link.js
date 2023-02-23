0b0be7b2be199c026f61fd9f7f3bcf8eed81d0f4
lauren0812 <106635328+lauren0812@users.noreply.github.com>
2023-02-22 18:26:13 -0700

    added github link and component page

---
+++ src/Link.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+export default function URL() {
+  return (
+    <p className="links">
+      <small>
+        <a
+          href="https://github.com/lauren0812/Weather-App_React.git"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Lauren Paff, hosted by{" "}
+        <a href="https://www.netlify.com/" target="_blank">
+          Netlify
+        </a>
+        .
+      </small>
+    </p>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Search from "./Search";
-import Hosted from "./Hosted";
+import Link from "./Link";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -10,6 +10,6 @@ root.render(
   <StrictMode>
     <h1>Weather Forecast</h1>
     <Search />
-    <Hosted />
+    <Link />
   </StrictMode>
 );
