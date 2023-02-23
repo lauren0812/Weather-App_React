ccf7a43c90a2e5f568ca76113601ca0a05a4015b
lauren0812 <106635328+lauren0812@users.noreply.github.com>
2023-02-22 17:57:29 -0700

    added hosted.js page and component to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Search from "./Search";
+import Hosted from "./Hosted";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +10,6 @@ root.render(
   <StrictMode>
     <h1>Weather Forecast</h1>
     <Search />
+    <Hosted />
   </StrictMode>
 );
