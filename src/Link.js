a15cd69f29ae6c5eb32fbe4d4d8a24a1eb79ef8b
lauren0812 <106635328+lauren0812@users.noreply.github.com>
2023-02-22 18:34:09 -0700

    deleted target blank from links

--- src/Link.js
+++ src/Link.js
@@ -4,16 +4,10 @@ export default function URL() {
   return (
     <p className="links">
       <small>
-        <a
-          href="https://github.com/lauren0812/Weather-App_React.git"
-          target="_blank"
-        >
+        <a href="https://github.com/lauren0812/Weather-App_React.git">
           Open-source code
         </a>{" "}
-        by Lauren Paff, hosted by{" "}
-        <a href="https://www.netlify.com/" target="_blank">
-          Netlify
-        </a>
+        by Lauren Paff, hosted by <a href="https://www.netlify.com/">Netlify</a>
         .
       </small>
     </p>
