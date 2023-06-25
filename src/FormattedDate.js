7c19efb895e6c3ff432e8714739f93fc126200e7
lauren0812 <106635328+lauren0812@users.noreply.github.com>
2023-06-25 14:32:13 -0600

    updated formatted date component.

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -19,5 +19,9 @@ export default function FormattedDate(props) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return <div>${day}</div>;
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
 }
--- src/Search.js
+++ src/Search.js
@@ -13,6 +13,7 @@ export default function Search() {
     setWeather({
       temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: Math.round(response.data.wind.speed),
